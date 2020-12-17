a2f426a3dffb357af2536f4ec55b837d48aa0eb6
ali.rostamian <alibalii2000@gmail.com>
2020-12-17 22:57:09 +0330

    last commit:adding second version of printArray method with String input and test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,7 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Integer[] intArray = {1, 2, 3, 4, 5};
         Double[] doubleArray = {1.1, 1.2, 1.3, 1.4};
         Character[] charArray = {'a', 'b', 'c', 'd', 'e'};
@@ -11,31 +10,39 @@ public class Main {
         printArray(doubleArray);
         printArray(charArray);
 
-        printArray(intArray,1,3);
-        printArray(doubleArray,1,3);
-        printArray(charArray,1,3);
+        printArray(intArray, 1, 3);
+        printArray(doubleArray, 1, 3);
+        printArray(charArray, 1, 3);
+
+        String[] stringArray = {"qwe","asd","xzc","fgh"};
+
+        printArray(stringArray);
 
     }
 
-    public static <E> void printArray(E[] input)
-    {
-        for (E element : input)
-        {
+    public static <E> void printArray(E[] input) {
+        for (E element : input) {
             System.out.print(element + ", ");
         }
         System.out.println();
     }
 
-    public static <E> void printArray(E[] input, int lowSubscript, int highSubscript)
-    {
-        if (lowSubscript<0||lowSubscript>input.length)
+    public static <E> void printArray(E[] input, int lowSubscript, int highSubscript) {
+        if (lowSubscript < 0 || lowSubscript > input.length)
             throw new IllegalArgumentException("wrong subcript");
-        if (highSubscript<0||highSubscript>input.length)
+        if (highSubscript < 0 || highSubscript > input.length)
             throw new IllegalArgumentException("wrong subcript");
-        for (int i = 0; i < input.length; i++)
-        {
+        for (int i = 0; i < input.length; i++) {
             if (i <= highSubscript & i >= lowSubscript)
                 System.out.print(input[i] + ", ");
+        }
+        System.out.println();
+    }
+
+    public static void printArray(String[] input)
+    {
+        for (String s : input) {
+            System.out.print(s + ", ");
         }
         System.out.println();
     }
