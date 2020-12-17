1c0adf29615d888c474ccc2f8ebf7e8b4f1a9ccd
ali.rostamian <alibalii2000@gmail.com>
2020-12-17 22:51:33 +0330

    adding overload version of printArray (with low and high subcript) with test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,20 +4,40 @@ public class Main {
 
     public static void main(String[] args)
     {
-        Integer[] intArray = { 1, 2, 3, 4, 5 };
-        Double[] doubleArray = { 1.1, 1.2, 1.3, 1.4 };
-        Character[] charArray = { 'a', 'b', 'c', 'd', 'e' };
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        Double[] doubleArray = {1.1, 1.2, 1.3, 1.4};
+        Character[] charArray = {'a', 'b', 'c', 'd', 'e'};
         printArray(intArray);
         printArray(doubleArray);
         printArray(charArray);
 
+        printArray(intArray,1,3);
+        printArray(doubleArray,1,3);
+        printArray(charArray,1,3);
+
     }
+
     public static <E> void printArray(E[] input)
     {
-        for (E element:input)
+        for (E element : input)
         {
-            System.out.print(element+", ");
+            System.out.print(element + ", ");
         }
         System.out.println();
     }
+
+    public static <E> void printArray(E[] input, int lowSubscript, int highSubscript)
+    {
+        if (lowSubscript<0||lowSubscript>input.length)
+            throw new IllegalArgumentException("wrong subcript");
+        if (highSubscript<0||highSubscript>input.length)
+            throw new IllegalArgumentException("wrong subcript");
+        for (int i = 0; i < input.length; i++)
+        {
+            if (i <= highSubscript & i >= lowSubscript)
+                System.out.print(input[i] + ", ");
+        }
+        System.out.println();
+    }
+
 }
