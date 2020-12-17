da380fa8fd5763a165dcece9f3037371a81f7822
ali.rostamian <alibalii2000@gmail.com>
2020-12-17 22:43:51 +0330

    adding the first version of the printArray method with the test code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,20 @@ public class Main {
 
     public static void main(String[] args)
     {
+        Integer[] intArray = { 1, 2, 3, 4, 5 };
+        Double[] doubleArray = { 1.1, 1.2, 1.3, 1.4 };
+        Character[] charArray = { 'a', 'b', 'c', 'd', 'e' };
+        printArray(intArray);
+        printArray(doubleArray);
+        printArray(charArray);
 
+    }
+    public static <E> void printArray(E[] input)
+    {
+        for (E element:input)
+        {
+            System.out.print(element+", ");
+        }
+        System.out.println();
     }
 }
